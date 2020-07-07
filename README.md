494b9c8826527796e37dbbc066ef651a61ce56b7
augu0093 <augu0093@gmail.com>
2020-07-07 16:08:32 +0200

    Ready to test dataloader

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# CNN_computer-vision
+# computer-vision
+By August Semrau Andersen
+
+This project is an entry into the Kaggle competition 'Digit Recognizer'.  
+https://www.kaggle.com/c/digit-recognizer/data.  
+The goal of the competition is correctly classify handwritte numbers from the MNIST data set using computer vision.
+
+The intent with the project is to display proficiency in using Neural Netowrk structures in a practical context.  
+
+
---
+++ data_load.py
@@ -0,0 +1,74 @@
+"""
+This script loads the MNIST numbers data set for utilization.
+@AugustSemrau
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+
+def load_digit_mnist():
+
+    df_train = pd.read_csv(train_path)
+
+
+class DigitDataMNIST:
+
+    # Initialize
+    def __init__(self):
+        self.train_path = './data/train.csv'
+        self.test_path = './data/train.csv'
+        self.X_train = None
+        self.y_train = None
+        self.X_val = None
+        self.y_val = None
+        self.test_data = None
+        # self.transform = transform
+
+    def load_digit_mnist(self, test=False, val_data=False):
+        # Load training data and split label from data
+        train_data = pd.read_csv(self.train_path)
+        self.X_train = np.asarray(train_data.iloc[:, 1:]).reshape(-1, 1, 28, 28)  # .astype(float);
+        self.y_train = np.asarray(train_data.iloc[:, 0])
+
+        # Load test data
+        test_data = pd.read_csv(self.test_path)
+        self.test_data = np.asarray(test_data.iloc[:,:]).reshape(-1, 1, 28, 28)
+
+        # Return test data for Kaggle submission
+        if test:
+            return self.test_data
+
+        # Return training data
+        if val_data:
+            self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X_train, self.y_train, test_size=0.2)
+            return self.X_train, self.X_val, self.y_train, self.y_val
+        else:
+            return self.X_train, self.y_train
+
+    # Get shape of the data
+    def __XtShape__(self):
+        return self.X_train.shape
+
+    def __XvShape__(self):
+        return self.X_val.shape
+
+    def __YtShape__(self):
+        return self.y_train.shape
+
+    def __YvShape__(self):
+        return self.y_val.shape
+
+    def __TestShape__(self):
+        return self.test_data.shape
+
+    # def __getitem__(self, idx):
+    #     item = self.X[idx]
+    #     label = self.Y[idx]
+    #
+    #     if self.transform:
+    #         item = self.transform(item)
+    #
+    #     return (item, label)
