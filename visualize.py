1e3b615e78d8d601b216e9aedeff24702c644f63
augu0093 <augu0093@gmail.com>
2020-07-07 20:00:46 +0200

    Finished vizualising for now

--- visualize.py
+++ visualize.py
@@ -13,13 +13,22 @@ if __name__ == '__main__':
     # Define class for loading
     digit_data = DigitDataMNIST()
 
-    # Load data
-    X_train, X_val, y_train, y_val = digit_data.load_digit_mnist(test=False, val_data=True)
-    print(X_train[0])
+    # Load data for building testable model
+    X_train_s, X_val, y_train_s, y_val = digit_data.load_digit_mnist(test=False, val_data=True)
+    print(X_train_s.shape)
+    print(X_val.shape)
+    print(y_train_s.shape)
+    print(y_val.shape)
 
-    # Use matplotlib for generating image of digit
-    plt.imshow(X_train[0], cmap='Greys')
+    # Load data for building final model
+    X_train_l, y_train_l = digit_data.load_digit_mnist(test=False, val_data=False)
+    print(X_train_l.shape)
+    print(y_train_l.shape)
+
+    # Load test data for making predictions
+    X_test = digit_data.load_digit_mnist(test=True, val_data=False)
+    print(X_test.shape)
+
+    # Use matplotlib for generating an image of a given digit from the data
+    plt.imshow(X_train_s[0], cmap='Greys')
     plt.show()
-
-
-
